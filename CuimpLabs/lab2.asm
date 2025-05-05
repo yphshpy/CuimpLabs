c678cb27f9ddace11b0eb979cb56879c321289f6
Sergey Okishev <voin2013qqq@mail.ru>
2025-05-05 12:10:06 +0300

    lab2 done

--- CuimpLabs/lab2.asm
+++ CuimpLabs/lab2.asm
@@ -5,6 +5,7 @@
 	arr1 dd 0EE21h, 04299h, 09544h, 0545Ah;
 	arr2 dd 4 dup (?);
 	M dd ?;
+	R dd ?;
 
 .code
 ExitProcess PROTO STDCALL :DWORD
@@ -18,22 +19,69 @@ Start:
 		mov eax, [esi];
 		dec eax;
 		mov [ebx], eax;
-		add esi, 4;
+		add esi, 4;			// На 4 байта дальше по массиву
 		add ebx, 4;
 		loop for_loop;
 		
 	mov ebx, offset arr2;	// Переопределю ссылку на arr2
 
-	mov eax, [ebx];
+	mov eax, [ebx];			// X' & Z'
 	and eax, [ebx + 8];
 	mov [esi], eax;
 
-	mov eax, [ebx + 4];
+	mov eax, [ebx + 4];		// Y' xor Q'
 	xor eax, [ebx + 12];
 	mov [esi + 4], eax;
 
+	mov eax, [esi];			// or
+	or eax, [esi + 4];
 
+	cmp M, 0;
+	jge pp1_call;
+	jmp pp2_call;
+	
+	pp1 PROC
+		mov eax, M;
+		sar eax, 1;
+		add eax, 01344h;
+		mov R, eax;
+		ret;
+		
+	pp1 ENDP
+
+	pp2 PROC
+		mov eax, M;
+		sal eax, 1;
+		sub eax, 092Ch;
+		mov R, eax;
+		ret;
+
+	pp2 ENDP
+
+	pp1_call:
+		call pp1;
+		jmp continue;
+
+	pp2_call:
+		call pp2;
+		jmp continue;
+
+	continue:
+
+	cmp R, 031B8h;
+	jge R_ge;
+	jmp R_l;
+
+	R_ge:
+		mov eax, R;
+		add eax, 0221Bh;
+		jmp exit;
+
+	R_l:
+		mov eax, R;
+		xor eax, 0913Ch;
+		jmp exit;
 
 exit:
-Invoke ExitProcess,1
+Invoke ExitProcess, 0
 End Start
